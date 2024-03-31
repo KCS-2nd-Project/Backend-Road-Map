9cfce5be3613b0c49d4c2eeb2769d43da0756042
SemiHyeon <gustpal08@gmail.com>
2024-03-31 13:20:57 +0900

    - 마이페이지 생성 마이페이지 컨트롤러 수정

--- src/main/java/ncnk/make/backendroadmap/domain/controller/HomeController.java
+++ src/main/java/ncnk/make/backendroadmap/domain/controller/HomeController.java
@@ -2,11 +2,15 @@ package ncnk.make.backendroadmap.domain.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ncnk.make.backendroadmap.domain.entity.Member;
+import ncnk.make.backendroadmap.domain.exception.SessionNullPointException;
 import ncnk.make.backendroadmap.domain.security.auth.LoginUser;
 import ncnk.make.backendroadmap.domain.security.auth.dto.SessionUser;
+import ncnk.make.backendroadmap.domain.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 /**
  * 홈 Controller
@@ -35,8 +39,26 @@ public class HomeController {
         return "Login/login";
     }
 
+    private final MemberService memberService;
     @GetMapping("/myPage")
-    public String myPage(@LoginUser SessionUser user, Model model) {
+    public String myPage(@LoginUser SessionUser user, Model model,@SessionAttribute(name = "member", required = false) SessionUser sessionUser
+                         ) {
+        if (sessionUser == null) {
+            throw new SessionNullPointException("[ERROR] SessionUser is null");
+        }
+        Member member = memberService.findMemberByEmail(sessionUser.getEmail()); // 회원 검색
+        model.addAttribute("memberID", member.getMemberId());
+        model.addAttribute("Profile", member.getProfile());
+        model.addAttribute("Email", member.getEmail());
+        model.addAttribute("Name", member.getName());
+        model.addAttribute("NickName",member.getNickName());
+        model.addAttribute("Github", member.getGithub());
+        model.addAttribute("Level", member.getLevel());
+        model.addAttribute("Point",member.getPoint());
+        model.addAttribute("hard",member.getHard());
+        model.addAttribute("normal",member.getNormal());
+        model.addAttribute("easy",member.getEasy());
+
 
         return "myPage/myPage";
     }
